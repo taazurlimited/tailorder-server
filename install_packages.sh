926f8ae2e682341d59b64660de1be3e88446de4d
jiloysss <jangeles@bai.ph>
2020-04-29 18:36:33 +0800

    is finished status

---
+++ install_packages.sh
@@ -0,0 +1,10 @@
+sudo pip install pipenv
+sudo apt install python-backports.functools-lru-cache
+pipenv shell
+pip install flask
+pipenv install
+pip install python-escpos
+pip install --upgrade arabic-reshaper
+pip install python-bidi && pip install wand && sudo apt-get install libmagickwand-dev
+pip install wand
+sudo apt-get install libmagickwand-dev
